523bf91d1fc1c8d90a6db4af5850c8fdd2ccf0b4
choiejae <ejae8319@gmail.com>
2023-01-26 15:16:33 +0900

    feat: split d_listadd, listadd

--- libft/ft_d_lstadd_back.c
+++ libft/ft_d_lstadd_back.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_d_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: choiejae <choiejae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 16:48:02 by ilhna             #+#    #+#             */
-/*   Updated: 2023/01/26 15:11:26 by choiejae         ###   ########.fr       */
+/*   Updated: 2023/01/26 15:01:56 by choiejae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_d_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	**lstcp;
-	t_list	*lastnode;
+	t_list	*dummy_node;
 
 	lstcp = lst;
-	if (*lstcp == (void *)0)
-		*lstcp = new;
-	else
-	{
-		lastnode = ft_lstlast(*lstcp);
-		lastnode->next = new;
-	}
+	dummy_node = ft_lstlast(*lstcp);
+	dummy_node->prev->next = new;
+	new->prev = dummy_node->prev;
+	dummy_node->prev = new;
+	new->next = dummy_node;
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: choiejae <choiejae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:47:08 by ilhna             #+#    #+#             */
-/*   Updated: 2023/01/25 19:43:19 by choiejae         ###   ########.fr       */
+/*   Updated: 2023/01/26 15:11:43 by choiejae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_d_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
