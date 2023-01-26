888a4f7104645b60dc091fcecd39ecf78ac85580
choiejae <ejae8319@gmail.com>
2023-01-26 15:16:33 +0900

    feat: split d_listadd, listadd

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: choiejae <choiejae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 16:48:02 by ilhna             #+#    #+#             */
-/*   Updated: 2023/01/26 11:26:23 by choiejae         ###   ########.fr       */
+/*   Updated: 2023/01/26 15:11:26 by choiejae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,14 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	**lstcp;
-	t_list	*dummy_node;
+	t_list	*lastnode;
 
 	lstcp = lst;
 	if (*lstcp == (void *)0)
 		*lstcp = new;
 	else
 	{
-		dummy_node = ft_lstlast(*lstcp);
-		dummy_node->prev->next = new;
-		new->prev = dummy_node->prev;
-		dummy_node->prev = new;
-		new->next = dummy_node;
+		lastnode = ft_lstlast(*lstcp);
+		lastnode->next = new;
 	}
 }
