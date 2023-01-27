6dfde33ee89c86951b6426366560686b23c1aa85
choiejae <ejae8319@gmail.com>
2023-01-27 22:52:03 +0900

    feat: signal ctrl + D & readline '\0' case

--- srcs_mandatory/main.c
+++ srcs_mandatory/main.c
@@ -365,8 +365,6 @@ size_t	get_envp_count(char **system_envp)
 	return (len);
 }
 
-
-// int	g_is_sig_interupt;
 extern int	g_is_sig_interupt;
 void	sig_ctrl_c(int signal)
 {
@@ -397,6 +395,21 @@ void	set_signal()
 	signal(SIGQUIT, SIG_IGN);
 }
 
+void	check_buf(char **buf)
+{
+	if (*buf == NULL)
+	{
+		ft_putstr_fd("\x1b[1A", STDOUT_FILENO);
+		ft_putstr_fd("\033[19C", STDOUT_FILENO);
+		ft_putstr_fd("exit\n", 1);
+		exit(0);
+	}
+	if (**buf == '\0')
+	{
+		**buf ='\n';
+	}
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	char	*buf;
@@ -407,11 +420,11 @@ int main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	load_config(&config, envp);
-	
 	while (1)
 	{
 		set_signal();
 		buf = readline(PROMPT);
+		check_buf(&buf);
 		splited_cmd = ft_split(buf, ' ');
 		if (ft_strnstr(splited_cmd[0], "cd", 2))
 		{
