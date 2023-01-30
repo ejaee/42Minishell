3f2f9de991bf5d53352579227c6ba6239f550b12
IH-M2 <ilhae.na@gmail.com>
2023-01-30 21:47:17 +0900

    finish builtin - exit

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ilhna <ilhna@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 23:53:34 by ilhna             #+#    #+#             */
-/*   Updated: 2023/01/30 21:38:41 by ilhna            ###   ########.fr       */
+/*   Updated: 2023/01/30 21:41:40 by ilhna            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,71 +23,6 @@
 
 # define PROMPT BROWN"M O N G S H E L L$ "RESET
 # define PROMPT_NAME "M O N G S H E L L"
-
-# define ERR_EXIT_MANY_ARGS "exit: too many arguments"
-# define ERR_EXIT_NUMERIC "numeric argument required"
-
-# include "libft.h"
-
-# define RED	"\x1b[31m"
-# define GREEN	"\x1b[32m"
-# define YELLOW	"\x1b[33m"
-# define BLUE	"\x1b[34m"
-# define WHITE	"\x1b[0m"
-# define BROWN	"\e[38;5;137m"
-# define RESET	"\x1b[0m"
-
-# define PROMPT BROWN"M O N G S H E L L$ "RESET
-# define PROMPT_NAME "M O N G S H E L L"
-
-# define ERR_EXIT_MANY_ARGS "exit: too many arguments"
-# define ERR_EXIT_NUMERIC "numeric argument required"
-
-# include "libft.h"
-
-# define RED	"\x1b[31m"
-# define GREEN	"\x1b[32m"
-# define YELLOW	"\x1b[33m"
-# define BLUE	"\x1b[34m"
-# define WHITE	"\x1b[0m"
-# define BROWN	"\e[38;5;137m"
-# define RESET	"\x1b[0m"
-
-# define PROMPT BROWN"M O N G S H E L L$ "RESET
-# define PROMPT_NAME "M O N G S H E L L"
-
-# define ERR_EXIT_MANY_ARGS "exit: too many arguments"
-# define ERR_EXIT_NUMERIC "numeric argument required"
-
-# include "libft.h"
-
-# define RED	"\x1b[31m"
-# define GREEN	"\x1b[32m"
-# define YELLOW	"\x1b[33m"
-# define BLUE	"\x1b[34m"
-# define WHITE	"\x1b[0m"
-# define BROWN	"\e[38;5;137m"
-# define RESET	"\x1b[0m"
-
-# define PROMPT BROWN"M O N G S H E L L$ "RESET
-# define PROMPT_NAME "M O N G S H E L L"
-
-# define ERR_EXIT_MANY_ARGS "exit: too many arguments"
-# define ERR_EXIT_NUMERIC "numeric argument required"
-
-# include "libft.h"
-
-// color.h
-# define RED	"\x1b[31m"
-# define GREEN	"\x1b[32m"
-# define YELLOW	"\x1b[33m"
-# define BLUE	"\x1b[34m"
-# define WHITE	"\x1b[0m"
-# define BROWN	"\e[38;5;137m"
-# define RESET	"\x1b[0m"
-
-# define PROMPT BROWN"M O N G S H E L L$ "RESET
-# define PROMPT_NAME BROWN"M O N G S H E L L"RESET
 
 # define ERR_EXIT_MANY_ARGS "exit: too many arguments"
 # define ERR_EXIT_NUMERIC "numeric argument required"
