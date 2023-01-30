9654be98bbe3d3a3ea0ad9661b29b13b81c68238
choiejae <ejae8319@gmail.com>
2023-01-30 16:49:42 +0900

    refactor: Remove unnecessary elements

--- srcs_mandatory/main.c
+++ srcs_mandatory/main.c
@@ -466,7 +466,6 @@ void	sig_ctrl_c(int signal)
 	{
 		if (pid == -1)
 		{
-			printf("pid = -1\n");
 			if (rl_on_new_line() == -1)
 				exit(1);
 			rl_replace_line("", 1);
@@ -475,7 +474,6 @@ void	sig_ctrl_c(int signal)
 		}
 		else
 		{
-			printf("pid = ??\n");
 			ft_putstr_fd("\n", STDOUT_FILENO);
 		}
 	}
